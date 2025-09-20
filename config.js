55ca97bfe8daa74ab29382c947c3d2c506f6b6fa
Lweendo225 <lweendomwananimbwejr@gmail.com>
2025-09-21 01:25:05 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,14 +7,14 @@ function convertToBool(text, fault = "true") {
 }
 
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "mb4GnALb#UPRgFXW5vaV-RjG-qESdZ4YzSTbFEe88oi-JoyWhHhk",
-  OWNER_NUM: process.env.OWNER_NUM || "94726228243",
+  SESSION_ID: process.env.SESSION_ID || "NENO-XMD~efBUlYBZ#qoKI1kkCssXIH_3bUHvMhL4Dk6J5NpWu-HGNrAjaE-8",
+  OWNER_NUM: process.env.OWNER_NUM || "260763583092",
   PREFIX: process.env.PREFIX || ".",
   AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
   AUTO_REACT_STATUS: process.env.AUTO_REACT_STATUS || "true",
   MODE : process.env.MODE || "public", 
   AUTO_STATUS_LIKE: process.env.AUTO_STATUS_LIKE || "true", 
-  AUTO_RECORDING: convertToBool(process.env.AUTO_RECORDING || "false"), 
+  AUTO_RECORDING: convertToBool(process.env.AUTO_RECORDING || "true"), 
   ANTI_DELETE: convertToBool(process.env.ANTI_DELETE || "true"),
   AUTO_VOICE: process.env.AUTO_VOICE || "true",
   AUTO_CHAT_GF: process.env.AUTO_CHAT_GF || "true",
